158b4eb54d61c37e8ff5dc52df9cd7b7d5a247e3
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2025-02-14 19:01:54 -0600

    feat(sportify): add basic structure with header, main content and footer

---
+++ Sportify/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Bienvenido a Sportify</h1>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre nosotros</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h2>Contenido Principal</h2>
+        <p>Aquí va la información básica de del sitio web.</p>
+      </main>
+      <footer>
+        <p>2025 Sportify. Digital House.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
