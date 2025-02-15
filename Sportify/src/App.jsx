a75df301ea35fcb26c35982bb21646cb816eb382
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2025-02-14 19:41:48 -0600

    feat(sportify): create header component

--- Sportify/src/App.jsx
+++ Sportify/src/App.jsx
@@ -1,19 +1,10 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-      <header>
-        <h1>Bienvenido a Sportify</h1>
-        <nav>
-          <ul>
-            <li>Inicio</li>
-            <li>Sobre nosotros</li>
-            <li>Contacto</li>
-          </ul>
-        </nav>
-      </header>
+      <Header/>
       <main>
         <h2>Contenido Principal</h2>
         <p>Aquí va la información básica de del sitio web.</p>
@@ -22,7 +13,7 @@ function App() {
         <p>2025 Sportify. Digital House.</p>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Sportify/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "./styles/header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Logo</h1>
+      <nav>
+        <ul>
+          <li>Inicio</li>
+          <li>Sobre nosotros</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
